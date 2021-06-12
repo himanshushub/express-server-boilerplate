4569e779e8cb0c9ced858466ccb3f230505331fd
himanshushub <himanshushub8@gmail.com>
2021-06-13 03:19:56 +0530

    - invocation removed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,5 @@ const requireLogin = require("../middlewares/requireLogin");
 const authRoutesController = require("../controller/authRoutesController");
 
 module.exports = (app) => {
-  app.post(
-    "/api/authRoutes",
-    requireLogin,
-    authRoutesController.authenticate()
-  );
+  app.post("/api/authRoutes", requireLogin, authRoutesController.authenticate);
 };
