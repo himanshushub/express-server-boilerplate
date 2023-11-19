596735f64006c43cdd75d52576719391f9b11bd9
himanshushub <himanshushub8@gmail.com>
2023-11-19 14:10:11 +0530

    chore: default route

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,11 @@ app.use(express.json());
 authRoutes(app);
 userRoutes(app);
 
+app.use("/", (req, res) => {
+  res.send("Server running");
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App running... , http://localhost:${PORT}`);
+});
